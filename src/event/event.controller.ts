cb9bdda185e969cb3c17bd5369d14826a6fbb0ac
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-05-26 23:20:19 +0200

    start crud event

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
-import { EventService } from "./event.service";
-
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { EventService } from './event.service';
+import { Events } from '@prisma/client';
 
 @Controller('events')
 export class EventController {
-  constructor( private readonly eventService: EventService) {}
-  @Get('eventtest')
+  constructor(private readonly eventService: EventService) {}
+  @Get()
+  getAllEvents() {
+    return this.eventService.events();
+  }
+
+  @Get()
   getEvent() {
     return this.eventService.getEvent();
+  }
+
+  @Post()
+  event(@Body() data: Events) {
+    return this.eventService.event({ data });
   }
 }
--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { Events, Prisma } from '@prisma/client';
+
 
 @Injectable()
 export class EventService {
+  constructor(private prisma: PrismaService) { }
   getEvent() {
     return [
       {
@@ -9,5 +13,16 @@ export class EventService {
         title: 'Event 1'
       }
     ]
+  }
+  async events() {
+    return this.prisma.events.findMany();
+  }
+
+  async event({ data }: { data: Prisma.EventsCreateInput}) {
+    const event = await this.prisma.events.create({
+      data
+    })
+    console.log("voici event : ", event);
+    return event;
   }
 }
