43487385e5617e327e2641f182b56af4a65d5103
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-05-28 10:18:21 +0200

    fix: update event service

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -31,28 +31,35 @@ export class EventsService {
     }
   }
 
+  async updateEvent(id: string, data: CreateEventDto) {
+    try {
+      await this.prisma.events.update({
+        where: { id },
+        data: {
+          id: data.id,
+          title: data.title,
+          description: data.description,
+          startDate: data.startDate,
+          image: data.image,
+          endDate: data.endDate,
+          location: data.location,
+          author: data.author,
+          contact: data.contact,
+          user: { connect: { id: data.userId } },
+        },
+      });
+      return { message: 'Évènement mis à jour avec succès' };
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException(
+        "Une erreur est survenue lors de la mise à jour de l'évènement",
+      );
+    }
+  }
+
   async getEvent(id: string) {
     const event = await this.prisma.events.findUnique({
       where: { id },
-    });
-    return event;
-  }
-
-  async updateEvent(id: string, data: CreateEventDto) {
-    const event = await this.prisma.events.update({
-      where: { id },
-      data: {
-        id: data.id,
-        title: data.title,
-        description: data.description,
-        startDate: data.startDate,
-        image: data.image,
-        endDate: data.endDate,
-        location: data.location,
-        author: data.author,
-        contact: data.contact,
-        user: { connect: { id: '9aaf8bf0-c418-4521-a515-e8ce5c2c1ac4' } },
-      },
     });
     return event;
   }
