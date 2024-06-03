361b98b6696880c321f6a8838ca5bb11e2f63af8
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-06-03 10:16:22 +0200

    fix path for deploy

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { PrismaModule } from '@/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
