1abd08781a2b4a77c2823407556d6bbec2edd482
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-06-03 10:14:36 +0200

    test for deploy api

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto, CreateUserDtoType } from './dto/create-user.dto';
 
 @Injectable()
