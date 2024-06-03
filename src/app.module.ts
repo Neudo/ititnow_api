850d2ba8658c91a5a415f6697ca98e3b0db75ca2
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-06-03 10:00:09 +0200

    test fix gto deploy api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { FavoritesModule } from './favorites/favorites.module';
 import { EventsModule } from './events/events.module';
 import { WebhookModule } from './webhook/webhook.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FavoritesModule, EventsModule, WebhookModule, UsersModule, PrismaModule],
+  imports: [EventsModule, WebhookModule, UsersModule, PrismaModule],
   controllers: [],
   providers: [],
 })
